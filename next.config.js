42eb06eb97f3b03793fb81947bd8daab002cbc29
senjam008 <53621291+senjam008@users.noreply.github.com>
2021-06-06 18:25:15 +0530

    testttttt

--- next.config.js
+++ next.config.js
@@ -46,7 +46,3 @@ module.exports = withCommerceConfig({
 
 // Don't delete this console log, useful to see the commerce config in Vercel deployments
 console.log('next.config.js', JSON.stringify(module.exports, null, 2))
-eeeee
-
-adjasgdjadkdks
-gagdajhgajgadkj
